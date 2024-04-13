d73973e933b013b7f29a563d3f77859b483c85e2
AncientAritocrat <dcy200348@outlook.com>
2024-04-13 21:27:38 +0800

    viewsUpdate

--- djangoProject/IntegratedApp/views.py
+++ djangoProject/IntegratedApp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django import forms
 from urllib.parse import quote
 
-from utils.MySpider import spider
+from static.utils.MySpider import spider
 
 
 # Create your views here.
